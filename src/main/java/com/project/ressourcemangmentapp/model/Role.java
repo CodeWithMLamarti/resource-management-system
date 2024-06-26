112f0dc35f0c5e8e63f04bba106d87d6f3d51e1e
medlmt <med.lmt012@gmail.com>
2024-06-26 22:25:23 +0100

    Role Model

---
+++ src/main/java/com/project/ressourcemangmentapp/model/Role.java
@@ -0,0 +1,17 @@
+package com.project.ressourcemangmentapp.model;
+
+import jakarta.persistence.*;
+
+import java.util.Collection;
+
+@Entity
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    @ManyToMany(mappedBy = "roles")
+    private Collection<User> users;
+
+}
