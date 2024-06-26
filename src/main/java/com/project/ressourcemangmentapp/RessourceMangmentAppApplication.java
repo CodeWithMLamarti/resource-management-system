2b947599d23a0174e4c0731a260a0c4eb0b58c3b
medlmt <med.lmt012@gmail.com>
2024-06-26 22:21:41 +0100

    Add User Model

---
+++ src/main/java/com/project/ressourcemangmentapp/RessourceMangmentAppApplication.java
@@ -0,0 +1,13 @@
+package com.project.ressourcemangmentapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RessourceMangmentAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RessourceMangmentAppApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/project/ressourcemangmentapp/model/User.java
@@ -0,0 +1,43 @@
+package com.project.ressourcemangmentapp.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "serial_number")
+    private float serialNumber;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @ManyToMany
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(
+                    name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(
+                    name = "role_id", referencedColumnName = "id"))
+    private Collection<Role> roles;
+
+}
